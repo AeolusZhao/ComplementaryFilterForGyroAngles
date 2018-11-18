25a42f812385c0a93a4c5b903ffa22a5f7419cda
Jianfeng Zhao <AeolusZhao@users.noreply.github.com>
2018-11-18 20:08:47 +0100

    add log_0002 old data

--- process.m
+++ process.m
@@ -1,16 +1,16 @@
 clear all;
 
 %specify how many entries from the log file to read
-m = 1000;
+m = 10000;
 
 [time, accX, accY, accZ, gyroX, gyroY, gyroZ, dt] = readDataFromSensorLog(m);
 
-% AngleXFromGyro = cumsum(gyroX) * dt;
-% AngleYFromGyro = cumsum(gyroY) * dt;
-% AngleZFromGyro = cumsum(gyroZ) * dt;
-AngleXFromAcc = atan( accX./ sqrt(accY.^2 + accZ.^2) );
-AngleYFromAcc = atan( accY./ sqrt(accX.^2 + accZ.^2) );
-AngleZFromAcc = atan( accZ./ sqrt(accX.^2 + accY.^2) );
+AngleXFromGyro = cumsum(gyroX) * dt;
+AngleYFromGyro = cumsum(gyroY) * dt;
+AngleZFromGyro = cumsum(gyroZ) * dt;
+AngleXFromAcc = atand( accX./ sqrt(accY.^2 + accZ.^2) ) ; % degree
+AngleYFromAcc = atand( accY./ sqrt(accX.^2 + accZ.^2) ) ; % degree
+AngleZFromAcc = atand( accZ./ sqrt(accX.^2 + accY.^2) ) ; % degree
 
 highCoe = 0.98; 
 lowCoe = 0.02;
@@ -24,9 +24,9 @@ angleY(1) = highCoe * (gyroY(1)*dt) + lowCoe * AngleYFromAcc(1);
 angleZ = zeros(m, 1);
 angleZ(1) = highCoe * (gyroZ(1)*dt) + lowCoe * AngleZFromAcc(1);
 
-hSurface = plotCamera();
-origin = [0, 0, 0];
-direction = [0 1 0];
+%hSurface = plotCamera();
+%origin = [0, 0, 0];
+%direction = [1 1 1];
 
 for i = 2:m
  angleX(i) = highCoe * ( angleX(i-1) + gyroX(i) * dt ) + lowCoe * AngleXFromAcc(i);
@@ -34,12 +34,12 @@ for i = 2:m
  angleZ(i) = highCoe * ( angleZ(i-1) + gyroZ(i) * dt ) + lowCoe * AngleZFromAcc(i); 
  %somehow rotate using the previous pos as reference, thus here subtract
  %the previoius pos
- rotate(hSurface,direction, (angleY(i) - angleY(i-1)) );
+ %rotate(hSurface,direction, (angleY(i) - angleY(i-1)) );
  
 %  figure(2); plot(time, angleX, time, angleY, time, angleZ);
 %  legend('angleX','angleY', 'angleZ')
- drawnow
+   drawnow
 end
 
-figure(1); plot(time, angleX, time, angleY, time, angleZ);
-legend('angleX','angleY', 'angleZ')
+figure(1); plot(time, angleX, time, AngleXFromAcc, time, AngleXFromGyro);
+legend('angleX','AngleXFromAcc', 'AngleXFromGyro')
--- readDataFromSensorLog.m
+++ readDataFromSensorLog.m
@@ -3,14 +3,12 @@ function [time, accX, accY, accZ, gyroX, gyroY, gyroZ, dt] = readDataFromSensorL
 filename = 'log_0000.csv';
 rawData = readtable(filename); 
 
-
-
 time = rawData.unit__timestamp_ms_(1:m) / 1000; %seconds
-accX = rawData.bmi160_a_x_mg_(1:m);
-accY = rawData.bmi160_a_y_mg_(1:m);
-accZ = rawData.bmi160_a_z_mg_(1:m);
-gyroX = rawData.bmi160_g_x_mDeg_(1:m) / 1000; %degree
-gyroY = rawData.bmi160_g_y_mDeg_(1:m) / 1000; %degree
-gyroZ = rawData.bmi160_g_z_mDeg_(1:m) / 1000; %degree
+accX = rawData.bmi160_a_x_mg_(1:m) / 1000; %g
+accY = rawData.bmi160_a_y_mg_(1:m) / 1000; %g
+accZ = rawData.bmi160_a_z_mg_(1:m) / 1000; %g
+gyroX = rawData.bmi160_g_x_mDeg_(1:m) / 1000; %degree/s
+gyroY = rawData.bmi160_g_y_mDeg_(1:m) / 1000; %degree/s
+gyroZ = rawData.bmi160_g_z_mDeg_(1:m) / 1000; %degree/s
 
 dt = time(2) - time(1); % seconds
